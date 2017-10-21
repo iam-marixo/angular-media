bc0315e7b8b99a9ceb475b764a73be88e6c4bdfa
iam-marixo <marisolgarcia.11314@gmail.com>
2017-10-20 18:31:32 -0700

    push 10/19 work

---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+  title = 'Movie Lovers!';
+
+  ngOnInit() {}
+}
